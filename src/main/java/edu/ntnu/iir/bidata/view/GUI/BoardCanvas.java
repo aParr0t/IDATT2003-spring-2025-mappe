2956fe27e8a7fc585c0bf217092d2cb85e7d91e2
aParrot <a.lapenas04@gmail.com>
2025-03-28 12:01:08 +0100

    feat(GUI): added players to board
    BoardCanvas can now hold a list of players.
    In BoardCanvas, added setter for players property.
    BoardCanvas now has a drawPlayers method that draws the player pieces on the board.

--- src/main/java/edu/ntnu/iir/bidata/view/GUI/BoardCanvas.java
+++ src/main/java/edu/ntnu/iir/bidata/view/GUI/BoardCanvas.java
@@ -1,10 +1,18 @@
 package edu.ntnu.iir.bidata.view.GUI;
 
 import edu.ntnu.iir.bidata.model.Board;
+import edu.ntnu.iir.bidata.model.Player;
+import edu.ntnu.iir.bidata.model.Tile;
+import javafx.geometry.Point2D;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+
+import java.util.List;
 
 public abstract class BoardCanvas extends Canvas {
   protected final Board board;
+  protected List<Player> players = List.of();
 
   public BoardCanvas(Board board) {
     this.board = board;
@@ -19,5 +27,33 @@ public abstract class BoardCanvas extends Canvas {
     draw();
   }
 
+  public void setPlayers(List<Player> players) {
+    this.players = players;
+    draw();
+  }
+
   public abstract void draw();
+
+  protected void drawPlayers(List<Player> players) {
+    GraphicsContext gc = getGraphicsContext2D();
+
+    for (int i = 0; i < players.size(); i++) {
+      Player player = players.get(i);
+      Tile tile = board.getTile(player.getPosition());
+      double tileWidth = tile.getWidth();
+      double tileHeight = tile.getHeight();
+      List<Point2D> offsets = List.of(
+              new Point2D(0, 0),
+              new Point2D(tileWidth / 2, 0),
+              new Point2D(0, tileHeight / 2),
+              new Point2D(tileWidth / 2, tileHeight / 2)
+      );
+      Point2D offset = offsets.get(i);
+      Point2D playerPos = tile.getPosition().add(offset);
+      double playerSize = tileWidth * 0.5 * getWidth();
+      String imagePath = player.getPlayingPiece().getImagePath();
+      Image image = new Image(imagePath);
+      gc.drawImage(image, playerPos.getX() * getWidth(), playerPos.getY() * getHeight(), playerSize, playerSize);
+    }
+  }
 }
