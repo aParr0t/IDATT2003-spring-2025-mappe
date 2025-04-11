f3bb3276f5d90ae8f42aeb329613543e1c7cafed
aParrot <a.lapenas04@gmail.com>
2025-04-11 11:29:04 +0200

    fix: forgot to use renamed class BoardGameController

--- src/main/java/edu/ntnu/iir/bidata/Main.java
+++ src/main/java/edu/ntnu/iir/bidata/Main.java
@@ -1,10 +1,10 @@
 package edu.ntnu.iir.bidata;
 
-import edu.ntnu.iir.bidata.controller.BoardGameApp;
+import edu.ntnu.iir.bidata.controller.BoardGameController;
 
 public class Main {
   public static void main(String[] args) {
-    BoardGameApp app = new BoardGameApp();
+    BoardGameController app = new BoardGameController();
     app.setup();
     app.run();
   }
