3842db7dcd78b6484bec0d44f3751bc8a0709080
aParrot <a.lapenas04@gmail.com>
2025-04-30 16:54:58 +0200

    feat: create a factory for file readers/writers

---
+++ src/main/java/edu/ntnu/iir/bidata/filehandling/FileHandlerFactory.java
@@ -0,0 +1,46 @@
+package edu.ntnu.iir.bidata.filehandling;
+
+/**
+ * Factory for creating file readers and writers.
+ */
+public class FileHandlerFactory {
+    private FileHandlerFactory() {
+        // Private constructor to prevent instantiation
+    }
+
+    /**
+     * Creates a BoardFileReader for reading board files.
+     *
+     * @return a BoardFileReader
+     */
+    public static BoardFileReader createBoardFileReader() {
+        return new BoardFileReaderGson();
+    }
+
+    /**
+     * Creates a BoardFileWriter for writing board files.
+     *
+     * @return a BoardFileWriter
+     */
+    public static BoardFileWriter createBoardFileWriter() {
+        return new BoardFileWriterGson();
+    }
+
+    /**
+     * Creates a PlayerFileReader for reading player files.
+     *
+     * @return a PlayerFileReader
+     */
+    public static PlayerFileReader createPlayerFileReader() {
+        return new PlayerFileReaderCSV();
+    }
+
+    /**
+     * Creates a PlayerFileWriter for writing player files.
+     *
+     * @return a PlayerFileWriter
+     */
+    public static PlayerFileWriter createPlayerFileWriter() {
+        return new PlayerFileWriterCSV();
+    }
+}
