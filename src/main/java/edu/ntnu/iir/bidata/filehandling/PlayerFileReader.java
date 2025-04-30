8d9928545e44307108a5850706e49a2e47a0716e
aParrot <a.lapenas04@gmail.com>
2025-04-30 22:35:51 +0200

    feat: extend readPlayers method to include additional exception handling

--- src/main/java/edu/ntnu/iir/bidata/filehandling/PlayerFileReader.java
+++ src/main/java/edu/ntnu/iir/bidata/filehandling/PlayerFileReader.java
@@ -1,5 +1,8 @@
 package edu.ntnu.iir.bidata.filehandling;
 
+import edu.ntnu.iir.bidata.exceptions.CsvParsingException;
+import edu.ntnu.iir.bidata.exceptions.FileNotFoundException;
+import edu.ntnu.iir.bidata.exceptions.PlayerDataException;
 import edu.ntnu.iir.bidata.model.Player;
 
 import java.io.IOException;
@@ -16,6 +19,9 @@ public interface PlayerFileReader {
      * @param filePath the path to the file
      * @return the list of players read from the file
      * @throws IOException if an I/O error occurs
+     * @throws FileNotFoundException if the file does not exist
+     * @throws CsvParsingException if there is an error parsing the CSV
+     * @throws PlayerDataException if there is an error with the player data
      */
-    List<Player> readPlayers(Path filePath) throws IOException;
+    List<Player> readPlayers(Path filePath) throws IOException, FileNotFoundException, CsvParsingException, PlayerDataException;
 }
