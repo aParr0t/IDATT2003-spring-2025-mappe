eec2c8eeff3fac4c9b5f8add04f7de5401d493c0
aParrot <a.lapenas04@gmail.com>
2025-05-01 10:37:38 +0200

    refactor: delete unused method

--- src/main/java/edu/ntnu/iir/bidata/model/tileaction/MoveAction.java
+++ src/main/java/edu/ntnu/iir/bidata/model/tileaction/MoveAction.java
@@ -1,6 +1,5 @@
 package edu.ntnu.iir.bidata.model.tileaction;
 
-import edu.ntnu.iir.bidata.model.Player;
 
 public class MoveAction implements TileAction {
   private final int start;
@@ -9,10 +8,6 @@ public class MoveAction implements TileAction {
   public MoveAction(int start, int end) {
     this.start = start;
     this.end = end;
-  }
-
-  public void execute(Player player) {
-    player.setPosition(end);
   }
 
   public int getStart() {
